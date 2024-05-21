5fcf75029034e604dc6354323ca1c4793973c49b
KimYoonjae <rladbswo1124@gmail.com>
2024-05-21 09:44:57 +0900

    Add getSum

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,12 @@
-#include "pch.h"
+﻿#include "pch.h"
+#include "../Teamwork/calc.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, TestName2) {
+	Calc calc;
+	EXPECT_EQ(2, calc.getSum(1, 1));
 }
